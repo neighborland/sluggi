b537a30392497e6c10f0b37bdbaa2e26f5aa8261
Tee Parham <tee@neighborland.com>
2014-05-27 13:12:09 -0600

    Less code

--- lib/sluggi/history.rb
+++ lib/sluggi/history.rb
@@ -11,7 +11,7 @@ module Sluggi
       def find_slug!(slug)
         object = where(slug: slug).first || find_slugs(slug).first.try(:sluggable)
         unless object.is_a?(self)
-          raise(ActiveRecord::RecordNotFound.new("Couldn't find #{self.name} with 'slug'='#{slug}'"))
+          raise ActiveRecord::RecordNotFound, "Couldn't find #{self.name} with 'slug'='#{slug}'"
         end
         object
       end
