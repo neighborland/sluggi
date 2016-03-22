c8bc9652145c69da39f2bc01ca7aa7245d398271
Tee Parham <tee@neighborland.com>
2016-03-22 10:11:25 -0600

    Whitespace, alphabetize includes

--- lib/sluggi/slugged.rb
+++ lib/sluggi/slugged.rb
@@ -1,11 +1,12 @@
 module Sluggi
   module Slugged
     extend ActiveSupport::Concern
+
     included do
+      include Sluggi::Model
+      include Sluggi::ValidateExclusionOf
       include Sluggi::ValidatePresence
       include Sluggi::ValidateUniqueness
-      include Sluggi::ValidateExclusionOf
-      include Sluggi::Model
     end
   end
 end
